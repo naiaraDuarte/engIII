744210dd27750cfeb0ff225ae34979ccb18264ae
Liziane Tamm <lizianetamm@hotmail.com>
2021-06-06 16:53:11 -0300

    Alteraçoes DAO

--- src/main/java/br/com/fatecmc/geacad/model/domain/Aluno.java
+++ src/main/java/br/com/fatecmc/geacad/model/domain/Aluno.java
@@ -5,25 +5,29 @@ import java.util.Date;
 public class Aluno extends Pessoa {
     private String ra;
     private Turma turma;
-
-    public Aluno(String ra, Turma turma) {
-        this.ra = ra;
-        this.turma = turma;
-    }
-
-    public Aluno(String ra, Turma turma, String nome, String rg, String cpf, Date data_nascimento, String sexo) {
-        super(nome, rg, cpf, data_nascimento, sexo);
-        this.ra = ra;
-        this.turma = turma;
-    }
+    private Endereco endereco;
 
     public Aluno() {
     }
-    
-    public Aluno(String ra, Turma turma, String nome, String rg, String cpf, Date data_nascimento, String sexo, int id) {
-        super(nome, rg, cpf, data_nascimento, sexo, id);
+
+    public Aluno(String ra, Turma turma, Endereco endereco) {
         this.ra = ra;
         this.turma = turma;
+        this.endereco = endereco;
+    }
+
+    public Aluno(String ra, Turma turma, Endereco endereco, String nome, String telefone, String cpf, Date data_nascimento, String sexo) {
+        super(nome, telefone, cpf, data_nascimento, sexo);
+        this.ra = ra;
+        this.turma = turma;
+        this.endereco = endereco;
+    }
+
+    public Aluno(String ra, Turma turma, Endereco endereco, String nome, String telefone, String cpf, Date data_nascimento, String sexo, int id) {
+        super(nome, telefone, cpf, data_nascimento, sexo, id);
+        this.ra = ra;
+        this.turma = turma;
+        this.endereco = endereco;
     }
 
     public String getRa() {
@@ -41,8 +45,14 @@ public class Aluno extends Pessoa {
     public void setTurma(Turma turma) {
         this.turma = turma;
     }
-    
-   
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 
     
     
